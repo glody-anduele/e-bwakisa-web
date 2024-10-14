ed9aef5ca4eb2dfe5020ecabb8bfd7eff4d2213e
glody-anduele <server.intelect@gmail.com>
2024-10-14 11:33:00 +0100

    Add files via upload

---
+++ _app/immutable/entry/start.BKOvvKl0.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DprB2ygq.js";export{t as start};
---
+++ _app/immutable/nodes/6.ZkdmuakJ.js
@@ -0,0 +1,40 @@
+import{s as g,n as l}from"../chunks/scheduler.Ce_0Mfso.js";import{S as b,i as q,e as r,s as h,c as m,k as d,f,l as v,g as u,d as o}from"../chunks/index.BsCQ4CRC.js";function C(p){let i,n=`<div class="container"><div class="row"><div class="col-sm-8 m-auto"><h1>Privacy Policy</h1> <p>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vivamus magna justo, lacinia
+                    eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.</p></div></div></div>`,a,e,c=`<div class="container"><div class="row"><div class="col-lg-3"><nav class="privacy-nav"><ul><li><a class="nav-link scrollTo" href="#userLicense">User License</a></li> <li><a class="nav-link scrollTo" href="#disclaimer">Disclaimer</a></li> <li><a class="nav-link scrollTo" href="#limitations">Limitations</a></li> <li><a class="nav-link scrollTo" href="#governigLaw">Governing Law</a></li></ul></nav></div> <div class="col-lg-9"><div class="block"><div id="userLicense" class="policy-item"><div class="title"><h3>Use License</h3></div> <div class="policy-details"><p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                                Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Vivamus
+                                suscipit tortor eget felis porttitor volutpat. Proin eget tortor risus. Vivamus magna
+                                justo, lacinia eget consectetur sed, convallis at tellus. Curabitur aliquet quam id dui
+                                posuere blandit. Sed porttitor lectus nibh. Nulla quis lorem ut libero malesuada
+                                feugiat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur
+                                non nulla sit amet nisl tempus convallis quis ac lectus. Mauris blandit aliquet elit,
+                                eget tincidunt nibh pulvinara.</p> <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                                Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Vivamus
+                                suscipit tortor eget felis porttitor volutpat. Proin eget tortor risus. Vivamus magna
+                                justo, lacinia eget consectetur sed, convallis at tellus. Curabitur aliquet quam id dui
+                                posuere blandit. Sed porttitor lectus nibh. Nulla quis lorem ut libero malesuada
+                                feugiat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur
+                                non nulla sit amet nisl tempus convallis quis ac lectus. Mauris blandit aliquet elit,
+                                eget tincidunt nibh pulvinara.</p></div></div> <div id="disclaimer" class="policy-item"><div class="title"><h3>Disclaimer</h3></div> <div class="policy-details"><p>Donec rutrum congue leo eget malesuada. Nulla porttitor accumsan tincidunt. Vivamus
+                                suscipit tortor eget felis porttitor volutpat. Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                                aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Donec rutrum
+                                congue leo eget malesuada. Curabitur non nulla sit amet nisl tempus convallis quis ac
+                                lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.</p></div></div> <div id="limitations" class="policy-item"><div class="title"><h3>Limitations</h3></div> <div class="policy-details"><p>Donec rutrum congue leo eget malesuada. Nulla porttitor accumsan tincidunt. Vivamus
+                                suscipit tortor eget felis porttitor volutpat. Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                                aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Donec rutrum
+                                congue leo eget malesuada. Curabitur non nulla sit amet nisl tempus convallis quis ac
+                                lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.</p></div></div> <div id="governigLaw" class="policy-item"><div class="title"><h3>Governing Law</h3></div> <div class="policy-details"><p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                                Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Vivamus
+                                suscipit tortor eget felis porttitor volutpat. Proin eget tortor risus. Vivamus magna
+                                justo, lacinia eget consectetur sed, convallis at tellus. Curabitur aliquet quam id dui
+                                posuere blandit. Sed porttitor lectus nibh. Nulla quis lorem ut libero malesuada
+                                feugiat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur
+                                non nulla sit amet nisl tempus convallis quis ac lectus. Mauris blandit aliquet elit,
+                                eget tincidunt nibh pulvinara.</p> <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                                Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Vivamus
+                                suscipit tortor eget felis porttitor volutpat. Proin eget tortor risus. Vivamus magna
+                                justo, lacinia eget consectetur sed, convallis at tellus. Curabitur aliquet quam id dui
+                                posuere blandit. Sed porttitor lectus nibh. Nulla quis lorem ut libero malesuada
+                                feugiat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur
+                                non nulla sit amet nisl tempus convallis quis ac lectus. Mauris blandit aliquet elit,
+                                eget tincidunt nibh pulvinara.</p></div></div></div></div></div></div>`;return{c(){i=r("section"),i.innerHTML=n,a=h(),e=r("section"),e.innerHTML=c,this.h()},l(t){i=m(t,"SECTION",{class:!0,"data-svelte-h":!0}),d(i)!=="svelte-138bufr"&&(i.innerHTML=n),a=f(t),e=m(t,"SECTION",{class:!0,"data-svelte-h":!0}),d(e)!=="svelte-18dtxap"&&(e.innerHTML=c),this.h()},h(){v(i,"class","section page-title"),v(e,"class","privacy section pt-0")},m(t,s){u(t,i,s),u(t,a,s),u(t,e,s)},p:l,i:l,o:l,d(t){t&&(o(i),o(a),o(e))}}}class y extends b{constructor(i){super(),q(this,i,null,C,g,{})}}export{y as component};
